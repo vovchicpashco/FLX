f5a667245be1692fcb6781afcec72becaeb03c71
Volodymyr Pashko <vovchicpashco@gmail.com>
2019-02-05 23:42:24 +0200

    final

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,5 +1,3 @@
-
-
 var a = (prompt('Please, enter "a"','')); 
 var b = (prompt('Please, enter "b"',''));
 var c = (prompt('Please, enter "c"',''));
@@ -9,18 +7,18 @@ var x1 = ( -b + Math.sqrt(Discriminant) ) / (2 * a);
 var x2 = ( -b - Math.sqrt(Discriminant) ) / (2 * a);
 
 if (isNaN(parseInt(a)) || isNaN(parseInt(b)) || isNaN(parseInt(c))) {
-        alert("Invalid input data");
+        alert('Invalid input data');
       } else {
         if (Discriminant < 0) {
-          alert("No Solution"+"; Discriminant = "+Discriminant);
+          alert('No Solution'+'; Discriminant = '+Discriminant);
           
         } else if (Discriminant === 0) {
           Solution = -b / (2 * a);
-           alert("x = "+Solution+"; Discriminant = " + Discriminant);
+           alert('x = '+Solution+'; Discriminant = ' + Discriminant);
         } else {
             x1 = ( -b + Math.sqrt(Discriminant) ) / (2 * a),
             x2 = ( -b - Math.sqrt(Discriminant) ) / (2 * a);
-           alert("x1 = "+x1+"; x2 = "+x2+"; Discriminant = "+Discriminant);
+           alert('x1 = '+x1+'; x2 = '+x2+'; Discriminant = '+Discriminant);
         }
       }
  
