b986b6a1f0fcf788b6bbbe69d4eca551a30f5d5e
Volodymyr Pashko <vovchicpashco@gmail.com>
2019-02-10 22:42:25 +0200

    update

--- FLX_homework_7/homework/src/js/task1.js
+++ FLX_homework_7/homework/src/js/task1.js
@@ -2,41 +2,41 @@ const currentTime = new Date().getHours();
 const login = prompt("Please, enter your login");
 
 if (login === null || login === "") {
-  alert("Canceled");
+    alert("Canceled");
 } else {
-  if (login.length < 4) {
-    alert("I don't know any users having name length less than 4 symbols");
-  } else if (login === "User") {
-    const password = prompt("Enter your password");
-    if (password === null || password === "") {
-      alert("Canceled");
+    if (login.length < 4) {
+        alert("I don't know any users having name length less than 4 symbols");
+    } else if (login === "User") {
+        const password = prompt("Enter your password");
+        if (password === null || password === "") {
+            alert("Canceled");
+        } else {
+            if (password === "UserPass") {
+                alert(
+                    currentTime >= 20
+                        ? "Good evening, dear User!"
+                        : "Good day, dear User!"
+                );
+            } else {
+                alert("Wrong password");
+            }
+        }
+    } else if (login === "Admin") {
+        const password = prompt("Enter your password");
+        if (password === null || password === "") {
+            alert("Canceled");
+        } else {
+            if (password === "RootPass") {
+                alert(
+                    currentTime >= 20
+                        ? "Good evening, dear Admin!"
+                        : "Good day, dear Admin!"
+                );
+            } else {
+                alert("Wrong password");
+            }
+        }
     } else {
-      if (password === "UserPass") {
-        alert(
-          currentTime >= 20
-            ? "Good evening, dear User!"
-            : "Good day, dear User!"
-        );
-      } else {
-        alert("Wrong password");
-      }
+        alert("I don't know you!");
     }
-  } else if (login === "Admin") {
-    const password = prompt("Enter your password");
-    if (password === null || password === "") {
-      alert("Canceled");
-    } else {
-      if (password === "RootPass") {
-        alert(
-          currentTime >= 20
-            ? "Good evening, dear Admin!"
-            : "Good day, dear Admin!"
-        );
-      } else {
-        alert("Wrong password");
-      }
-    }
-  } else {
-    alert("I don't know you!");
-  }
 }
--- FLX_homework_7/homework/src/js/task2.js
+++ FLX_homework_7/homework/src/js/task2.js
@@ -3,61 +3,61 @@ const globals = {
     bid: 10,
     prize: 0,
     defaultRange: 5
-  };
-  
-  let random = Object.assign({}, globals);
-  let playGame = true;
-  if (confirm("Do you want to play a game?")) {
+};
+
+let random = Object.assign({}, globals);
+let playGame = true;
+if (confirm("Do you want to play a game?")) {
     while (playGame) {
-      const luckyNum = Math.floor(Math.random() * random.defaultRange);
-      playGame = false;
-      for (
-        let attempts = random.tries, currentBid = random.bid;
-        attempts > 0;
-        attempts--, currentBid = Math.floor(currentBid / 2)
-      ) {
-        const guess = parseInt(
-          prompt(`Enter a number from 0 to ${random.defaultRange}
+        const luckyNum = Math.floor(Math.random() * random.defaultRange);
+        playGame = false;
+        for (
+            let attempts = random.tries, currentBid = random.bid;
+            attempts > 0;
+            attempts-- , currentBid = Math.floor(currentBid / 2)
+        ) {
+            const guess = parseInt(
+                prompt(`Enter a number from 0 to ${random.defaultRange}
 Attempts left: ${attempts}
 Total prize: ${random.prize}$
 Possible prize on current attempt: ${currentBid}$`)
-        );
-        if (guess === luckyNum) {
-          random.prize += currentBid;
-          random.defaultRange += random.defaultRange;
-          random.bid *= 3;
-          if (
-            confirm(`
+            );
+            if (guess === luckyNum) {
+                random.prize += currentBid;
+                random.defaultRange += random.defaultRange;
+                random.bid *= 3;
+                if (
+                    confirm(`
               Congratulation! Your prize is: ${random.prize}$.
               Do you want to continue?`)
-          ) {
-            playGame = true;
-            break;
-          } else {
-            alert(`Thank you for a game. Your prize is: ${random.prize}$`);
-            if (confirm("Do you want to play again?")) {
-              random.prize = globals.prize;
-              random.defaultRange = globals.defaultRange;
-              random.bid = globals.bid;
-            playGame = true;
-              break;
+                ) {
+                    playGame = true;
+                    break;
+                } else {
+                    alert(`Thank you for a game. Your prize is: ${random.prize}$`);
+                    if (confirm("Do you want to play again?")) {
+                        random.prize = globals.prize;
+                        random.defaultRange = globals.defaultRange;
+                        random.bid = globals.bid;
+                        playGame = true;
+                        break;
+                    }
+                    break;
+                }
+            } else if (isNaN(guess)) {
+                alert("You did not become a millionaire, but can.");
+                break;
+            } else if (attempts <= 1) {
+                alert(`Thank you for a game. Your prize is: ${random.prize}$`);
+                random.prize = globals.prize;
+                random.defaultRange = globals.defaultRange;
+                random.bid = globals.bid;
+                playGame =
+                    confirm("Do you want to play again?") ||
+                    alert("You did not become a millionaire, but can.");
             }
-            break;
-          }
-        } else if (isNaN(guess)) {
-          alert("You did not become a millionaire, but can.");
-          break;
-        } else if (attempts <= 1) {
-          alert(`Thank you for a game. Your prize is: ${random.prize}$`);
-          random.prize = globals.prize;
-          random.defaultRange = globals.defaultRange;
-          random.bid = globals.bid;
-        playGame =
-            confirm("Do you want to play again?") ||
-            alert("You did not become a millionaire, but can.");
         }
-      }
     }
-  } else {
+} else {
     alert("You did not become a millionaire, but can.");
-  }
+}
