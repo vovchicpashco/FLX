14ade0cbdf14630e714a119d21ef6f621486b913
Volodymyr Pashko <vovchicpashco@gmail.com>
2019-02-10 21:26:51 +0200

    First commit

---
+++ FLX_homework_7/homework/src/js/task1.js
@@ -0,0 +1,42 @@
+const currentTime = new Date().getHours();
+const login = prompt("Please, enter your login");
+
+if (login === null || login === "") {
+  alert("Canceled");
+} else {
+  if (login.length < 4) {
+    alert("I don't know any users having name length less than 4 symbols");
+  } else if (login === "User") {
+    const password = prompt("Enter your password");
+    if (password === null || password === "") {
+      alert("Canceled");
+    } else {
+      if (password === "UserPass") {
+        alert(
+          currentTime >= 20
+            ? "Good evening, dear User!"
+            : "Good day, dear User!"
+        );
+      } else {
+        alert("Wrong password");
+      }
+    }
+  } else if (login === "Admin") {
+    const password = prompt("Enter your password");
+    if (password === null || password === "") {
+      alert("Canceled");
+    } else {
+      if (password === "RootPass") {
+        alert(
+          currentTime >= 20
+            ? "Good evening, dear Admin!"
+            : "Good day, dear Admin!"
+        );
+      } else {
+        alert("Wrong password");
+      }
+    }
+  } else {
+    alert("I don't know you!");
+  }
+}
