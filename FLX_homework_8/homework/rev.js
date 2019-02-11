2ab4604c2ef5f8ce629a3d539e17edc50acbf9b7
Volodymyr Pashko <vovchicpashco@gmail.com>
2019-02-12 01:58:14 +0200

    homework 8

--- FLX_homework_8/homework/rev.js
+++
@@ -1,7 +0,0 @@
-var n = prompt('gfggg', 0)
-function reverse() {  
-  return Number(Array.from(String(Math.abs(n))).reverse().join('')) * Math.sign(n);
-}
-
-console.log(reverse(144));
-console.log(reverse(-90000));
