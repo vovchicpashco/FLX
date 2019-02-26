e5616d60f0bb70b13c8bdc3be9af128f9c31f6e5
Volodymyr Pashko <vovchicpashco@gmail.com>
2019-02-26 20:15:41 +0200

    fixed js

--- FLX_homework_12/homework/src/app.js
+++ FLX_homework_12/homework/src/app.js
@@ -1,222 +1,241 @@
 const rootNode = document.getElementById('root');
-
-// const todoItems = [
-//     {isDone: false, id: 12345, description: 'Todo 1'}
-// ];
-
 let todoItems = [];
-let i = 0;
-let id;
-let item;
 
-function addListItem(description) {
-    if (description) {
-        id = i++;
-        item = { isDone: false, id, description };
+const storage = {
+    addItem(description) {
+        const id = 'task' + new Date().getTime();
+        const item = { id, description, isDone: false };
         todoItems.push(item);
         localStorage.setItem('todoItems', JSON.stringify(todoItems));
-        console.log('item.id ' + item.id);
+
+        return todoItems;
+    },
+
+    getAll() {
+        return JSON.parse(localStorage.getItem('todoItems'));
+    },
+
+    getById(id) {
+        return this.getAll().find(item => item.id === id);
+    },
+
+    removeById(id) {
+        const updatedList = this.getAll().filter(item => item.id !== id);
+
+        localStorage.setItem('todoItems', JSON.stringify(updatedList));
+
+        return todoItems;
+    },
+
+    getDone() {
+        return this.getAll().filter(item => item.isDone === true);
+    },
+
+    getUndone() {
+        return this.getAll().filter(item => item.isDone === false);
+    },
+    setAsDoneById(id) {
+        const updatedList = this.getAll().map(item => {
+            if (item.id === id) {
+                item.isDone = true;
+            }
+
+            return item;
+        });
+
+        localStorage.setItem('todoItems', JSON.stringify(updatedList));
+
+        return todoItems;
+    },
+
+    changeDescription(id, descr) {
+        const updatedList = this.getAll().map(item => {
+            if (item.id === id) {
+                item.description = descr;
+            }
+
+            return item;
+        });
+
+        localStorage.setItem('todoItems', JSON.stringify(updatedList));
+
+        return todoItems;
+    },
+
+    getSorted() {
+        return this.getUndone().concat(this.getDone());
     }
-    return todoItems;
 }
 
+function createElement(tag, attributes, innerText) {
+    const newElem = document.createElement(tag);
 
-function addTask() {
-    let container = document.createElement('div');
-    container.className = 'containerAdd';
-
-    let mainContent = rootNode.appendChild(container);
-
-    let title = document.createElement('h1');
-    title.innerText = 'Add task';
-
-    mainContent.appendChild(title);
-
-    let inputField = document.createElement('input');
-    inputField.type = 'text';
-
-    mainContent.appendChild(inputField);
-
-    let buttonWrapper = document.createElement('div');
-    buttonWrapper.className = 'buttonWrapper';
-
-    mainContent.appendChild(buttonWrapper);
-
-
-    let canselButton = document.createElement('div');
-    canselButton.className = 'button';
-    canselButton.innerText = 'Cansel';
-    buttonWrapper.appendChild(canselButton);
-    canselButton.addEventListener('click', () => {
-        location.hash = '';
-        // history.go();
-    });
-
-    let saveButton = document.createElement('div');
-    saveButton.className = 'button';
-    saveButton.innerText = 'Save changes';
-    buttonWrapper.appendChild(saveButton);
-    saveButton.addEventListener('click', () => {
-        addListItem(inputField.value.trim());
-        location.hash = '';
-    });
-
-    rootNode.appendChild(mainContent);
-    return mainContent;
-}
-
-function modifyItem(item) {
-    let container = document.createElement('div');
-    container.className = 'containerMod';
-
-    let mainContent = rootNode.appendChild(container);
-
-    let title = document.createElement('h1');
-    title.innerText = 'Modify item';
-
-    mainContent.appendChild(title);
-
-    let inputField = document.createElement('input');
-    inputField.type = 'text';
-
-    mainContent.appendChild(inputField);
-
-    let buttonWrapper = document.createElement('div');
-    buttonWrapper.className = 'buttonWrapper';
-
-    mainContent.appendChild(buttonWrapper);
-
-
-    let canselButton = document.createElement('div');
-    canselButton.className = 'button';
-    canselButton.innerText = 'Cansel';
-    buttonWrapper.appendChild(canselButton);
-    canselButton.addEventListener('click', () => {
-        location.hash = '';
-        // history.go();
-    });
-
-    let saveButton = document.createElement('div');
-    saveButton.className = 'button save';
-    saveButton.innerText = 'Save changes';
-    buttonWrapper.appendChild(saveButton);
-
-    mainContent.querySelector('.save').onclick = () => {
-        changeDescription(item.id, inputField.value.trim());
-        console.log(inputField.value.trim());
-        location.hash = '';
-    };
-
-    // saveButton.addEventListener("click", () => {
-    // 	// alert(1);
-    // 	changeDescription(item.id, inputField.value.trim());
-    //    console.log(inputField.value.trim());
-    //    location.hash = '';
-    //  });
-
-    rootNode.appendChild(mainContent);
-    return mainContent;
-}
-
-function preview(todoItems) {
-
-    let container = document.createElement('div');
-    container.className = 'container';
-
-    let mainContent = rootNode.appendChild(container);
-
-    let title = document.createElement('h1');
-    title.innerText = 'Simple TODO Aplication';
-
-    let button = document.createElement('div');
-    button.innerText = 'Add new task';
-    button.className = 'button';
-
-    button.addEventListener('click', () => {
-        location.hash += '#addTask';
-    });
-
-
-    let listWrapper = document.createElement('ul');
-    mainContent.appendChild(title);
-    mainContent.appendChild(button);
-    mainContent.appendChild(listWrapper);
-
-    if (todoItems.length) {
-        for (let item of todoItems) {
-            // debugger;
-            let li = document.createElement('li');
-            li.setAttribute('id', item.id);
-            let removeButton = document.createElement('button');
-            removeButton.className = 'removeButton';
-
-            let text = document.createElement('button');
-            text.innerText = item.description;
-
-            text.addEventListener('click', () => {
-                location.hash += '#modifyItem';
-            });
-
-            removeButton.addEventListener('click', () => {
-                li.remove();
-                // removeById(item.id);
-            });
-
-            li.appendChild(text);
-            li.appendChild(removeButton);
-            listWrapper.appendChild(li);
+    if (Object.keys(attributes).length) {
+        for (let key in attributes) {
+            if (attributes.hasOwnProperty(key)) {
+                newElem.setAttribute(key, attributes[key]);
+            }
         }
-    } else {
-        let article = document.createElement('p');
-        article.innerText = 'TODO is empty';
-        mainContent.appendChild(article);
     }
 
+    if (innerText) {
+        newElem.appendChild(document.createTextNode(innerText));
+    }
 
-    return mainContent;
-
+    return newElem;
 }
 
-function getItemById(id) {
-    return localStorage.getItem('todoItems').find(item => item.id === id);
-}
+const template = {
+    addItemPage() {
+        const section = createElement('section', { 'class': 'add' });
+        const header = createElement('h1', {}, 'Add task');
+        const footer = createElement('footer', { 'class': 'add' });
+        const input = createElement('input', { 'class': 'input', 'type': 'text', 'placeholder': 'Task description' });
+        const saveBtn = createElement('button', { 'class': 'save-btn', 'disabled': 'true' }, 'Save changes');
+        const cancelBtn = createElement('button', { 'class': 'cancel-btn' }, 'Cancel');
 
-function removeFromLocalStorage(id) {
-    let remove = localStorage.getItem('todoItems').filter(item => item.id !== id);
-    console.log(localStorage.setItem('todoItems', JSON.stringify(remove)));
+        section.appendChild(header);
+        section.appendChild(input);
+        footer.appendChild(cancelBtn);
+        footer.appendChild(saveBtn);
+        section.appendChild(footer);
 
-    return todoItems;
-}
+        input.onchange = input.onkeyup = () => {
+            const description = input.value.trim();
 
-function changeDescription(id, description) {
-    let newDescription = JSON.parse(localStorage.getItem('todoItems')).map(item => {
-        if (item.id === id) {
-            item.description = description;
+            saveBtn.disabled = !description;
         }
-        console.log(item);
-        return item;
-    });
 
-    let a = localStorage.setItem('todoItems', JSON.stringify(newDescription));
+        saveBtn.addEventListener('click', () => {
+            storage.addItem(input.value.trim());
+            window.location.hash = '/main';
 
-    console.log(todoItems, JSON.stringify(newDescription));
+        });
 
+        cancelBtn.addEventListener('click', () => {
+            window.location.hash = '/main';
+        });
 
+        return section;
+    },
 
-    return todoItems;
-}
+    modifyItemPage(item) {
+        const section = this.addItemPage();
 
-function hide() {
-    rootNode.innerHTML = '';
-    if (location.hash.endsWith('#modifyItem')) {
-        rootNode.appendChild(modifyItem(item));
-    } else if (location.hash.endsWith('#addTask')) {
-        rootNode.appendChild(addTask());
-        // console.log(item);
-    } else {
-        rootNode.appendChild(preview(todoItems));
+        section.querySelector('h1').innerText = 'Modify item';
+        section.querySelector('input').value = item.description;
+
+        section.querySelector('.save-btn').onclick = () => {
+            storage.changeDescription(item.id, section.querySelector('input').value.trim());
+            window.location.hash = '/main'
+        };
+
+        return section;
+    },
+
+    mainPage(todoItems) {
+        const section = createElement('section', { 'class': 'main' });
+        const header = createElement('h1', {}, 'Simple TODO application');
+        const button = createElement('button', { 'class': 'addNewTask-btn' }, 'Add new task');
+        const p = createElement('p', { 'class': 'emptyList' }, 'TODO is empty');
+        const todoList = createElement('ul', { 'class': 'todoList' });
+
+        if (todoItems.length) {
+            for (let item of todoItems) {
+                const li = createElement('li', { 'id': item.id });
+                const checkbox = createElement('button',
+                    { 'class': item.isDone ? 'checkbox-done' : 'checkbox-undone' });
+                const text = createElement('button', { 'class': 'todo-text' }, item.description);
+                const remove = createElement('button', { 'class': 'remove-btn' });
+
+                checkbox.onclick = () => {
+                    if (checkbox.className === 'checkbox-undone') {
+                        checkbox.className = 'checkbox-done';
+                        storage.setAsDoneById(item.id);
+                        text.classList.add('marked');
+                        todoList.appendChild(li);
+                    } else {
+                        checkbox.className = 'checkbox-undone';
+                    }
+
+                };
+
+                remove.onclick = () => {
+                    li.remove();
+                    storage.removeById(item.id);
+                };
+
+                text.onclick = () => {
+                    window.location.hash = `/modify/${item.id}`;
+                };
+
+                li.appendChild(checkbox);
+                li.appendChild(text);
+                li.appendChild(remove);
+                todoList.appendChild(li);
+            }
+        }
+
+        button.addEventListener('click', () => {
+            window.location.hash = '/add';
+        });
+
+        section.appendChild(header);
+        section.appendChild(button);
+        section.appendChild(todoList);
+        section.appendChild(p);
+
+        return section;
     }
 }
 
-window.addEventListener('hashchange', hide);
-rootNode.appendChild(preview(todoItems));
+
+
+const route = {
+    load() {
+        const hash = window.location.hash;
+
+        if (hash.endsWith('/add')) {
+            this.add();
+        } else if (hash.endsWith('/main') || !hash) {
+            this.main(todoItems);
+        } else {
+            const id = hash.slice(hash.lastIndexOf('/') + 1);
+            this.modify(id);
+        }
+    },
+
+    main() {
+        document.title = 'Main Page';
+
+        rootNode.innerHTML = '';
+        rootNode.appendChild(template.mainPage(todoItems));
+
+    },
+
+    add() {
+        document.title = 'Add New Task';
+
+        rootNode.innerHTML = '';
+        rootNode.appendChild(template.addItemPage());
+
+    },
+
+    modify(id) {
+        const item = storage.getById(id);
+
+        document.title = 'Modify';
+
+        rootNode.innerHTML = '';
+        rootNode.appendChild(template.modifyItemPage(item));
+
+    }
+};
+
+
+window.onload = window.onhashchange = () => {
+    if (localStorage.getItem('todoItems')) {
+        todoItems = storage.getSorted();
+    }
+    route.load();
+};
